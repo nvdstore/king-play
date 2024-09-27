ac715d71b3a58d3bdeeb1fe63b97ca6b64586dc5
Erik <moherik.maulana@corp.bm.co.id>
2024-09-27 14:30:36 +0700

    workaround db issue

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,12 @@
+import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import pg from 'pg';
 
 export const db = new pg.Pool({
-	host: env.DATABASE_HOST,
-	user: env.DATABASE_USER,
-	password: env.DATABASE_PASSWORD,
-	database: env.DATABASE_NAME,
+	host: building ? '' : env.DATABASE_HOST,
+	user: building ? '' : env.DATABASE_USER,
+	password: building ? '' : env.DATABASE_PASSWORD,
+	database: building ? '' : env.DATABASE_NAME,
 	max: 20,
 	idleTimeoutMillis: 30000,
 	connectionTimeoutMillis: 2000
